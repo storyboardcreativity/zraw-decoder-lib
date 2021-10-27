9ef6e4ce4764ed9d36e73a9b549cd2e19f885879
xhrust <storyboardcreativity@gmail.com>
2021-10-27 14:34:52 -0400

    C interface changed
    - Removed redundant macros
    - Implemented decoder state enum
    - Now any operation with decoder returns state
    - Implemented "zraw_decoder__get_hisi_frame_info" function that returns read frame info (user can get frame info even before decoding it)

--- include/libzraw.h
+++ include/libzraw.h
@@ -1,25 +1,46 @@
+#include <stdint.h>
+
 typedef void* ZRAW_DECODER_HANDLE;
 
 #ifdef _MSC_VER
-    // Microsoft 
-    #define EXPORT __declspec(dllexport)
-    #define IMPORT __declspec(dllimport)
+    // Microsoft
+    #ifdef LIB_ZRAW_COMPILING
+    #   define LIB_ZRAW_PUBLIC __declspec(dllexport)
+    #else
+    #   define LIB_ZRAW_PUBLIC __declspec(dllimport)
+    #endif
 #elif __GNUC__
     // GCC
-    #define EXPORT __attribute__((visibility("default")))
-    #define IMPORT
+    #ifdef LIB_ZRAW_COMPILING
+    #   define LIB_ZRAW_PUBLIC __attribute__((visibility("default")))
+    #else
+    #   define LIB_ZRAW_PUBLIC
+    #endif
 #else
     // do nothing and hope for the best?
-    #define EXPORT
-    #define IMPORT
-    #pragma warning Unknown dynamic link import/export semantics.
+    #pragma error Unknown dynamic link import/export semantics.
 #endif
 
-#ifdef LIB_ZRAW_COMPILING
-#   define LIB_ZRAW_PUBLIC EXPORT
-#else
-#   define LIB_ZRAW_PUBLIC IMPORT
-#endif
+#pragma pack(push, 1)
+typedef struct zraw_frame_info_s
+{
+    uint32_t width_in_photodiodes;
+    uint32_t height_in_photodiodes;
+    uint32_t bits_per_photodiode_value;
+
+} zraw_frame_info_t;
+#pragma pack(pop)
+
+typedef enum zraw_decoder_state_e
+{
+    ZRAW_DECODER_STATE__INVALID_INSTANCE = 0,
+    ZRAW_DECODER_STATE__STANDBY,
+    ZRAW_DECODER_STATE__FRAME_IS_READ,
+    ZRAW_DECODER_STATE__FRAME_READING_FAILED,
+    ZRAW_DECODER_STATE__FRAME_IS_DECOMPRESSED,
+    ZRAW_DECODER_STATE__FRAME_DECOMPRESSION_FAILED,
+    ZRAW_DECODER_STATE__INSTANCE_IS_REMOVED
+} zraw_decoder_state_t;
 
 #ifdef __cplusplus
 extern "C" {
@@ -29,13 +50,16 @@ extern "C" {
     LIB_ZRAW_PUBLIC ZRAW_DECODER_HANDLE zraw_decoder__create();
 
     // Reads ZRAW frame
-    LIB_ZRAW_PUBLIC void zraw_decoder__read_hisi_frame(ZRAW_DECODER_HANDLE decoder, void* data, int max_size);
+    LIB_ZRAW_PUBLIC zraw_decoder_state_t zraw_decoder__read_hisi_frame(ZRAW_DECODER_HANDLE decoder, void* data, int max_size);
+
+    // Returns current ZRAW frame info
+    LIB_ZRAW_PUBLIC zraw_decoder_state_t zraw_decoder__get_hisi_frame_info(ZRAW_DECODER_HANDLE decoder, zraw_frame_info_t& info_out);
 
     // Processes initialized frame data
-    LIB_ZRAW_PUBLIC void zraw_decoder__decompress_hisi_frame(ZRAW_DECODER_HANDLE decoder);
+    LIB_ZRAW_PUBLIC zraw_decoder_state_t zraw_decoder__decompress_hisi_frame(ZRAW_DECODER_HANDLE decoder);
 
     // Frees decoder
-    LIB_ZRAW_PUBLIC void zraw_decoder__free(ZRAW_DECODER_HANDLE decoder);
+    LIB_ZRAW_PUBLIC zraw_decoder_state_t zraw_decoder__free(ZRAW_DECODER_HANDLE decoder);
 
 #ifdef __cplusplus
 }
--- src/interface.cpp
+++ src/interface.cpp
@@ -27,36 +27,57 @@ extern "C" LIB_ZRAW_PUBLIC ZRAW_DECODER_HANDLE zraw_decoder__create()
 }
 
 // Reads ZRAW frame
-extern "C" LIB_ZRAW_PUBLIC void zraw_decoder__read_hisi_frame(ZRAW_DECODER_HANDLE decoder, void* data, int max_size)
+extern "C" LIB_ZRAW_PUBLIC zraw_decoder_state_t zraw_decoder__read_hisi_frame(ZRAW_DECODER_HANDLE decoder, void* data, int max_size)
 {
     auto it = g_zraw_decoding_contexts.find(decoder);
     if (it == g_zraw_decoding_contexts.end())
-        return;
+        return ZRAW_DECODER_STATE__INVALID_INSTANCE;
     
     // Create stream for data
     std::istrstream databuf_stream(reinterpret_cast<const char *>(data), max_size);
 
     // Parse ZRAW structure
     ZRawFrameContainerParserSingletone::Instance().ParseFrame(it->second->frame__get(), databuf_stream);
+
+    return ZRAW_DECODER_STATE__FRAME_IS_READ;
+}
+
+extern "C" LIB_ZRAW_PUBLIC zraw_decoder_state_t zraw_decoder__get_hisi_frame_info(ZRAW_DECODER_HANDLE decoder, zraw_frame_info_t& info_out)
+{
+    auto it = g_zraw_decoding_contexts.find(decoder);
+    if (it == g_zraw_decoding_contexts.end())
+        return ZRAW_DECODER_STATE__INVALID_INSTANCE;
+
+    auto decoder_instance = it->second;
+
+    info_out.width_in_photodiodes = decoder_instance->frame__get().Parameters().frame_width;
+    info_out.height_in_photodiodes = decoder_instance->frame__get().Parameters().frame_height;
+    info_out.bits_per_photodiode_value = 8 + decoder_instance->frame__get().Parameters().frame_bitdepth * 2;
+
+    return ZRAW_DECODER_STATE__STANDBY;
 }
 
 // Processes initialized frame data
-extern "C" LIB_ZRAW_PUBLIC void zraw_decoder__decompress_hisi_frame(ZRAW_DECODER_HANDLE decoder)
+extern "C" LIB_ZRAW_PUBLIC zraw_decoder_state_t zraw_decoder__decompress_hisi_frame(ZRAW_DECODER_HANDLE decoder)
 {
     auto it = g_zraw_decoding_contexts.find(decoder);
     if (it == g_zraw_decoding_contexts.end())
-        return;
+        return ZRAW_DECODER_STATE__INVALID_INSTANCE;
 
     ZRawFrameDecompressorSingletone::Instance().DecompressFrame(it->second->frame__get());
+
+    return ZRAW_DECODER_STATE__FRAME_IS_DECOMPRESSED;
 }
 
 // Frees decoder
-extern "C" LIB_ZRAW_PUBLIC void zraw_decoder__free(ZRAW_DECODER_HANDLE decoder)
+extern "C" LIB_ZRAW_PUBLIC zraw_decoder_state_t zraw_decoder__free(ZRAW_DECODER_HANDLE decoder)
 {
     auto it = g_zraw_decoding_contexts.find(decoder);
     if (it == g_zraw_decoding_contexts.end())
-        return;
+        return ZRAW_DECODER_STATE__INVALID_INSTANCE;
 
     delete it->second;
     g_zraw_decoding_contexts.erase(it);
+
+    return ZRAW_DECODER_STATE__INSTANCE_IS_REMOVED;
 }
