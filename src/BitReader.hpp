2c418b17f64d43169af5d4e38e907650db3e6d87
unknown <storyboardcreativity@gmail.com>
2022-10-04 03:11:07 +0300

    Refactoring
    - Renamed "_curr_byte" to "_curr_bytes" (now size is uint64_t, can be any other size)

--- src/BitReader.hpp
+++ src/BitReader.hpp
@@ -10,8 +10,8 @@ class BitReader
 public:
     BitReader(std::istream &stream, bool cached)
         : _stream(stream),
-          _curr_byte(0x00),
-          _bits_left_in_curr_byte(0),
+          _curr_bytes(0x00),
+          _bits_left_in_curr_bytes(0),
           _position_in_bits(0),
           _cached_bytes_curr_pos(0),
           _cached(cached)
@@ -26,7 +26,7 @@ public:
     {
         if (bit_count == 0)
             return 0;
-        
+
         if (bit_count > sizeof(uint64_t) * 8)
             throw new std::invalid_argument("bit_count can't be more than 64!");
 
@@ -46,12 +46,12 @@ public:
     {
         if (value == 0)
             return;
-        
+
         uint32_t mod = _position_in_bits % value;
         if (mod)
             FlushBits(value - mod);
     }
-    
+
     void SeekToBit(uint64_t bit_index)
     {
         uint64_t byte_index = bit_index / 8;
@@ -61,13 +61,13 @@ public:
             _stream.seekg(byte_index, _stream.beg);
 
         // Reset counters and refresh current byte
-        _bits_left_in_curr_byte = 0;
+        _bits_left_in_curr_bytes = 0;
         _position_in_bits = bit_index;
         _refreshCurrentByte();
 
         // Skip last bits
-        _curr_byte >>= (bit_index % 8);
-        _bits_left_in_curr_byte -= bit_index % 8;
+        _curr_bytes >>= (bit_index % 8);
+        _bits_left_in_curr_bytes -= (bit_index % 8);
     }
 
     uint64_t ShowBits(uint32_t bit_count)
@@ -94,19 +94,22 @@ private:
     void _refreshCurrentByte()
     {
         // If we have no bits left in current byte
-        if (_bits_left_in_curr_byte == 0)
+        if (_bits_left_in_curr_bytes == 0)
         {
             // Read next byte
             if (_cached)
             {
-                _curr_byte = _cached_bytes_curr_pos >= _cached_bytes.size() ? 0x00 : _cached_bytes[_cached_bytes_curr_pos];
-                ++_cached_bytes_curr_pos;
+                for (int i = 0; i < sizeof(_curr_bytes); ++i)
+                {
+                    ((uint8_t*)&_curr_bytes)[i] = _cached_bytes_curr_pos >= _cached_bytes.size() ? 0x00 : _cached_bytes[_cached_bytes_curr_pos];
+                    ++_cached_bytes_curr_pos;
+                }
             }
             else
-                _stream.read((char *)&_curr_byte, 1);
+                _stream.read((char *)&_curr_bytes, sizeof(_curr_bytes));
 
             // Refresh left bits counter
-            _bits_left_in_curr_byte = 8;
+            _bits_left_in_curr_bytes = sizeof(_curr_bytes) * 8;
         }
     }
 
@@ -116,13 +119,13 @@ private:
         _refreshCurrentByte();
 
         // Get current bit
-        bool result = _curr_byte & 0x01;
+        bool result = _curr_bytes & 0x01;
 
         // Make next bit current
-        _curr_byte >>= 1;
+        _curr_bytes >>= 1;
 
         // Refresh bit counters
-        --_bits_left_in_curr_byte;
+        --_bits_left_in_curr_bytes;
         ++_position_in_bits;
 
         // Return saved bit
@@ -130,8 +133,8 @@ private:
     }
 
     std::istream &_stream;
-    uint8_t _curr_byte;
-    int _bits_left_in_curr_byte;
+    uint64_t _curr_bytes;
+    int _bits_left_in_curr_bytes;
     uint64_t _position_in_bits;
 
     std::vector<char> _cached_bytes;
