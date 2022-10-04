b9364cc3e10a9f8387b81f0081634db8311f3efc
unknown <storyboardcreativity@gmail.com>
2022-10-04 20:30:39 +0300

    Optimized BitReader

--- src/BitReader.hpp
+++ src/BitReader.hpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 #include <vector>
 #include <cstdint>
+#include <cstring>
 
 class BitReader
 {
@@ -19,7 +20,10 @@ public:
         _stream.seekg(0, _stream.beg);
 
         if (cached)
+        {
             _cached_bytes = std::vector<char>((std::istreambuf_iterator<char>(_stream)), std::istreambuf_iterator<char>());
+            _cached_size = _cached_bytes.size();
+        }
     }
 
     uint64_t ReadBits(uint32_t bit_count)
@@ -30,15 +34,20 @@ public:
         if (bit_count > sizeof(uint64_t) * 8)
             throw new std::invalid_argument("bit_count can't be more than 64!");
 
-        uint64_t value = 0;
-        for (uint32_t i = 0; i < bit_count; ++i)
-            value |= (((uint64_t)_getNextBit()) << i);
-
-        return value;
+        return _getBits(bit_count);
     }
 
     void FlushBits(uint32_t bit_count)
     {
+        // Fast variant if we have cached bits
+        if (bit_count <= _bits_left_in_curr_bytes)
+        {
+            _bits_left_in_curr_bytes -= bit_count;
+            _position_in_bits += bit_count;
+            _curr_bytes >>= bit_count;
+            return;
+        }
+
         SeekToBit(CurrentPositionInBits() + bit_count);
     }
 
@@ -63,7 +72,9 @@ public:
         // Reset counters and refresh current byte
         _bits_left_in_curr_bytes = 0;
         _position_in_bits = bit_index;
-        _refreshCurrentByte();
+
+        // Here we know that (_bits_left_in_curr_bytes == 0) - use unsafe function
+        _refreshCurrentByteUnsafe();
 
         // Skip last bits
         _curr_bytes >>= (bit_index % 8);
@@ -72,6 +83,15 @@ public:
 
     uint64_t ShowBits(uint32_t bit_count)
     {
+        // Fast variant if we have cached bits
+        if (bit_count <= _bits_left_in_curr_bytes)
+        {
+            uint64_t res = _curr_bytes;
+            res <<= (64 - bit_count);
+            res >>= (64 - bit_count);
+            return res;
+        }
+
         // Save current position
         uint64_t bits_before = _position_in_bits;
 
@@ -95,22 +115,28 @@ private:
     {
         // If we have no bits left in current byte
         if (_bits_left_in_curr_bytes == 0)
-        {
-            // Read next byte
-            if (_cached)
-            {
-                for (int i = 0; i < sizeof(_curr_bytes); ++i)
-                {
-                    ((uint8_t*)&_curr_bytes)[i] = _cached_bytes_curr_pos >= _cached_bytes.size() ? 0x00 : _cached_bytes[_cached_bytes_curr_pos];
-                    ++_cached_bytes_curr_pos;
-                }
-            }
-            else
-                _stream.read((char *)&_curr_bytes, sizeof(_curr_bytes));
+            _refreshCurrentByteUnsafe();
+    }
 
-            // Refresh left bits counter
-            _bits_left_in_curr_bytes = sizeof(_curr_bytes) * 8;
+    void _refreshCurrentByteUnsafe()
+    {
+        // Read next byte
+        if (_cached)
+        {
+            _curr_bytes = 0;
+            int32_t left = (_cached_size - _cached_bytes_curr_pos);
+            int32_t sz_to_read = left > ((int32_t)sizeof(_curr_bytes)) ? sizeof(_curr_bytes) : left;
+
+            if (sz_to_read > 0)
+                std::memcpy(&_curr_bytes, &_cached_bytes.data()[_cached_bytes_curr_pos], sz_to_read);
+
+            _cached_bytes_curr_pos += sizeof(_curr_bytes);
         }
+        else
+            _stream.read((char *)&_curr_bytes, sizeof(_curr_bytes));
+
+        // Refresh left bits counter
+        _bits_left_in_curr_bytes = sizeof(_curr_bytes) * 8;
     }
 
     bool _getNextBit()
@@ -132,12 +158,57 @@ private:
         return result;
     }
 
+    uint64_t _getBits(uint64_t bit_count)
+    {
+        // Refresh current byte in case we have no bits left
+        _refreshCurrentByte();
+
+        if (bit_count > _bits_left_in_curr_bytes)
+        {
+            uint64_t res_a = _curr_bytes;
+            res_a <<= (64 - _bits_left_in_curr_bytes);
+            res_a >>= (64 - _bits_left_in_curr_bytes);
+            uint64_t res_a_sz = _bits_left_in_curr_bytes;
+
+            bit_count -= _bits_left_in_curr_bytes;
+            _position_in_bits += _bits_left_in_curr_bytes;
+            _bits_left_in_curr_bytes = 0;
+
+            // Here we know that (_bits_left_in_curr_bytes == 0) - use unsafe function
+            _refreshCurrentByteUnsafe();
+
+            uint64_t res_b = _curr_bytes;
+            res_b <<= (64 - bit_count);
+            res_b >>= (64 - bit_count);
+
+            _bits_left_in_curr_bytes -= bit_count;
+            _position_in_bits += bit_count;
+
+            _curr_bytes >>= bit_count;
+
+            return (res_a | (res_b << res_a_sz));
+        }
+
+        uint64_t res = _curr_bytes;
+        res <<= (64 - bit_count);
+        res >>= (64 - bit_count);
+
+        _bits_left_in_curr_bytes -= bit_count;
+        _position_in_bits += bit_count;
+
+        _curr_bytes >>= bit_count;
+
+        return res;
+    }
+
     std::istream &_stream;
     uint64_t _curr_bytes;
     int _bits_left_in_curr_bytes;
     uint64_t _position_in_bits;
 
     std::vector<char> _cached_bytes;
+    int64_t _cached_size;
+
     int _cached_bytes_curr_pos;
     bool _cached;
 };
